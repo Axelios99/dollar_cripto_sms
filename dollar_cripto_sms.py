91a5c6c2e78af85d197f0bffdcf3abbd170c69f9
Axelios99 <alexanderladino99@hotmail.com>
2023-01-20 12:34:14 -0500

    Create get_df_criptos and create get_dollar_today

--- dollar_cripto_sms.py
+++ dollar_cripto_sms.py
@@ -11,6 +11,24 @@ from datetime import datetime
 import requests
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from twilio.rest import Client
+from scripts_config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, PHONE_NUMBER_SMS, PHONE_NUMBER_WHATSAPP, EXCHANGE_TOKEN
+from utils import get_dollar_today, get_df_criptos
+import pandas as pd
+import numpy as np
 
-url_dollar = 'https://www.datos.gov.co/resource/32sa-8pi3.json'
-response = requests.get(url_dollar).json()
+
+
+url_dollar = "https://api.apilayer.com/exchangerates_data/convert?to=COP&from=USD&amount=1"
+url_criptos = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Cdogecoin%2Ctether%2Ccardano%2Ctron%2Cchainlink%2Ceos%2Cdai%2Cfrax&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24'
+
+
+
+price_dollar = get_dollar_today(url_dollar)
+
+df_criptos = get_df_criptos(url_criptos)
+
+
+
+    
+
